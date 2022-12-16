8c3ec906b68c65475b6ec4dfd59ffc2d689fe7c1
Harsh Tyagi <harshtyagi9897u@gmail.com>
2022-12-16 14:03:56 +0530

    Main code

--- src/com/company/AnnualBalance.java
+++ src/com/company/AnnualBalance.java
@@ -2,8 +2,8 @@ package com.company;
 
 public class AnnualBalance {
     public static void main(String[] args){
-        double balance = 783489.987;
-        double interestRate = 15.23;
+        double balance = 1000;
+        double interestRate = 3.5;
         double interest = balance * (interestRate/1200);
         System.out.println("Interest: "+interest);
     }
