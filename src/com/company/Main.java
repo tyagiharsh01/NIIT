9fc0ce11629ac6772f8cac97b29cd249536bad91
Harsh Tyagi <harshtyagi9897u@gmail.com>
2022-12-16 10:07:23 +0530

    Main code

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+           int english =78;
+           int maths=68;
+           int science = 67;
+           int hindi = 86;
+           int computer=92;
+           int total_marks = english+hindi+maths+science+computer;
+           double avegare_marks = total_marks/5.0;
+            System.out.println("English: "+english);
+            System.out.println("Hindi: "+hindi);
+            System.out.println("Maths: "+maths);
+            System.out.println("Science: "+science);
+            System.out.println("Computer: "+computer);
+            System.out.println("Total Marks: "+total_marks);
+            System.out.println("Average_Marks: "+avegare_marks);
+    }
+
+}
