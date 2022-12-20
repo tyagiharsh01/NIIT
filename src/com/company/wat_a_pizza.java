d872a3988f1562912358baf0f4c637093724f5ec
Harsh Tyagi <harshtyagi9897u@gmail.com>
2022-12-20 21:51:31 +0530

    some changes done

--- src/com/company/wat_a_pizza.java
+++ src/com/company/wat_a_pizza.java
@@ -3,41 +3,58 @@ package com.company;
 import java.util.Scanner;
 
 public class wat_a_pizza {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        String email = sc.nextLine();
-        long contactNumber = sc.nextLong();
-        sc.nextLine();
-        String  address = sc.nextLine();
-        System.out.println(address);
+            public static void main(String[] args){
+                int choice;
+                Scanner sc = new Scanner(System.in);
+                String name = sc.nextLine();
+                String email = sc.nextLine();
+                long contactNumber = sc.nextLong();
+                sc.nextLine();
+                String  address = sc.nextLine();
+                float pizzaPrice = 12.99f;
+                float garlicBreadPrice = 5.99f;
+                float beveragePrice = 1.99f;
+                float total_bill=0;
+                //	System.out.println("Enter 1 for Pizza, 2 for Garlic Bread and 3 for Beverages");
 
-        float pizzaPrice = 12.99f;
-        float garlicBreadPrice = 5.99f;
-        float beveragePrice = 1.99f;
-        //	System.out.println("Enter 1 for Pizza, 2 for Garlic Bread and 3 for Beverages");
-        int n = sc.nextInt();
-        switch(n){
-            case 1 :
-                int noOfPizzasOrdered = sc.nextInt();
-                float totalPizzaPrice = noOfPizzasOrdered*pizzaPrice;
-                System.out.println("Your total Pizza bill : $ "+totalPizzaPrice);
-                break;
-            case 2:
-                int noOfGarlicBreadOrdered  = sc.nextInt();
-                float totalGarlicBreadPrice = noOfGarlicBreadOrdered*garlicBreadPrice;
-                System.out.println("Your total Garlic Bread Bill : $ "+totalGarlicBreadPrice);
-                break;
-            case 3:
-                int noOfBeveragesOrdered = sc.nextInt();
-                float totalbeveragesPrice = noOfBeveragesOrdered*beveragePrice;
-                System.out.println("Your total Beverage Bill : $ "+totalbeveragesPrice);
-                break;
-            default:
-                System.out.println("Please provide a valid input");
-                break;
+                do{
+                    int n = sc.nextInt();
+                    switch(n){
+                        case 1 :
+                            int noOfPizzasOrdered = sc.nextInt();
+                            float totalPizzaPrice = noOfPizzasOrdered*pizzaPrice;
+                            total_bill +=totalPizzaPrice;
+                            break;
+                        case 2:
+                            int noOfGarlicBreadOrdered  = sc.nextInt();
+                            float totalGarlicBreadPrice = noOfGarlicBreadOrdered*garlicBreadPrice;
+                            total_bill = total_bill+totalGarlicBreadPrice;
+                            break;
+                        case 3:
+                            int noOfBeveragesOrdered = sc.nextInt();
+                            float totalbeveragesPrice = noOfBeveragesOrdered*beveragePrice;
+                            total_bill = total_bill+totalbeveragesPrice;
+                            break;
+                        default:
+                            System.out.println("Please provide a valid input");
+                            break;
+                    }
+                    choice = sc.nextInt();
+                }while(choice==1);
+                System.out.println("The total bill amount : $ "+total_bill);
+                if(total_bill>50){
+                    float discounted_bill = total_bill+(total_bill*10)/100;
+                    System.out.println("The total discounted bill amount : $ "+discounted_bill);
+                }
+                else{
+                    System.out.println("No discount provided since bill amount is less than $ 50");
+                }
 
+
+
+            }
         }
-    }
-    }
+
+
+
 
